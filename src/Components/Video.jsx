2d2d9c50d406cf16d433e1f0387b952e84c7267a
Rustamov Nodir <nodirr5555@gmail.com>
2024-08-05 14:49:32 +0500

    editVideo

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -47,7 +47,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { FaCircleChevronRight } from "react-icons/fa6";
 
-import videoHome from "../assets/videoHome.mp4";
+import videoHome from "../assets/videoMuzey.mp4";
 
 gsap.registerPlugin(ScrollTrigger);
 
